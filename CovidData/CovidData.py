3961d1e1c06e25bd0572ad793b76f7de448cea2a
Azhar Yousuf <33654256+Zorrom@users.noreply.github.com>
2022-05-11 20:13:30 +0530

    Covid Data collector
    This script collects Covid data for the last 3 days from a selected country

---
+++ CovidData/CovidData.py
@@ -0,0 +1,96 @@
+'''
+This code fetches Covid data for last 3 days of a particular country
+'''
+from itertools import islice
+from bs4 import BeautifulSoup
+from urllib.request import Request, urlopen
+from ExcelAndDict import countries
+
+
+class CountrySelection:
+    def country_option(self):
+        for key, value in countries.countryvalues.items():
+            print(key + " " + ":" + " " + value)
+        user_selected_country = input("Please select your country: ")
+        print("")
+        try:
+            return countries.countryvalues[user_selected_country]
+        except KeyError:
+            print("Sorry that wasn't a valid option, please select a value "
+                  "only from the available options")
+            exit(1)
+
+
+class CovidDataCollector:
+    def __init__(self, country):
+        self.country = country
+
+    def web_scraper(self):
+        req = Request('https://www.worldometers.info/coronavirus/#countries', headers={'User-Agent': 'Mozilla/5.0'})
+        webpage = urlopen(req).read()
+        html = webpage.decode("utf-8")
+        soup = BeautifulSoup(html, "html.parser")
+        data_obtained = soup.get_text()
+        return data_obtained
+
+    def store_data(self, data_obtained):
+        with open(outputFile, "w", encoding="utf-8") as f:
+            f.write(data_obtained)
+
+    def covid_count(self):
+        case_list = []
+        with open(outputFile, "r") as f:
+            for line in f:
+                if self.country in line:
+                    val = ''.join(islice(f, 2))
+                    if "+" in val:
+                        cases = val
+                        final_count = cases.partition('\n')[-1]
+                        case_list.append(final_count)
+        converted_list = []
+        for element in case_list:
+            converted_list.append(element.strip())
+        return converted_list
+
+    def int_to_str(self, plus_list):
+        string_list = []
+        for value in plus_list:
+            cases_in_string = ''.join(ch for ch in value if ch.isalnum())
+            string_list.append(cases_in_string)
+        return string_list
+
+    def cases_count_per_country(self, cases):
+        updated_list = cases
+        size = len(cases)
+        if size == 1:
+            updated_list.insert(0, 0)
+            updated_list.insert(1, 0)
+            print("Only data from last two days are available")
+        elif size == 2:
+            updated_list.insert(0, 0)
+            print("Today's data hasn't been received yet")
+        elif size == 3:
+            print("All data are available")
+        return updated_list
+
+    def print_covid_data(self, finaldata):
+        dates = ["Today", "Yesterday", "2 days ago"]
+        print(f'Below are the covid details of {self.country} for the past 3 days')
+        for data, date in zip(finaldata, dates):
+            print(f'Covid cases as of {date} is {data}')
+
+
+def execute():
+    country_class = CountrySelection()
+    country_fetch = country_class.country_option()
+    covid = CovidDataCollector(country_fetch)
+    mydata = covid.web_scraper()
+    covid.store_data(mydata)
+    active_cases = covid.covid_count()
+    string_list = covid.int_to_str(active_cases)
+    final_list = covid.cases_count_per_country(string_list)
+    covid.print_covid_data(final_list)
+
+
+outputFile = 'data.txt'
+execute()
